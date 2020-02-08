760a5f563357114a943ec21cc407d7ff3bc52764
Chloe Foronda <caforonda103@yahoo.com>
2020-02-08 05:17:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,9 @@ class Triangle
   end
   
   def kind
-    if side1 <= 0, side2 <= 0, side3 <= 0
+    if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
+      raise TriangleError
+      elsif 
       
   
 
