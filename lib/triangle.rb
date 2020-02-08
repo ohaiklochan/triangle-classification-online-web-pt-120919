b1df39d39edf1aef9fa17ef1f9440ea244312f73
Chloe Foronda <caforonda103@yahoo.com>
2020-02-08 05:15:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,6 +3,12 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
   
   def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def triangle(kind)
     
   
 
