1111d848a48d2347e547fb9bfee55ab08cb62143
Chloe Foronda <caforonda103@yahoo.com>
2020-02-08 05:12:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,13 @@
 class Triangle
 
   attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(kind)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+  
 
   class TriangleError < StandardError
     
