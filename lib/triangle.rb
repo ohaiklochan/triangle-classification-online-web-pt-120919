9b9699aaae19b622f1a71ba18f454d39f87b6aaa
Chloe Foronda <caforonda103@yahoo.com>
2020-02-08 05:21:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,9 @@ class Triangle
     elsif (@side1+@side2 <= @side3) || (@side1+@side3 <= @side2) || (@side2+@side3 <= @side1)
       raise TriangleError
     else
-      
+      if (@side1 == @side2) && (@side2 == @side3) && (@side1 == @side3)
+        :equilateral
+      elsif (@side )
       
   
 
